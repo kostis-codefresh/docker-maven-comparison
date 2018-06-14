112e997baedec1dc2c9899d008f7f550edc716c8
Kostis Kapelonis <kostis@codefresh.io>
2018-06-14 10:31:29 -0400

    Made integration tests targetable

--- 03-multistage/src/test/groovy/com/codepipes/wizard/HomePageSpec.groovy
+++ 03-multistage/src/test/groovy/com/codepipes/wizard/HomePageSpec.groovy
@@ -10,30 +10,58 @@ import spock.lang.*
  * @author Kostis
  *
  */
-class HomePageSpec extends Specification {
-	
-	@Shared
-	def client = new RESTClient("http://localhost:8080/wizard/")
+ class HomePageSpec extends Specification {
 
-	def "Homepage exists"() {
-		when: "a rest call is performed to the main page"
-		def response = client.get(path : "index.html")
-		
-		then: "the correct message is expected"
-		with(response) {
-			status == 200
-		}
-	}
-	
-	def "About Page exists"() {
-		when: "a rest call is performed to the about page"
-		def response = client.get(path : "about.html")
-		
-		then: "the correct message is expected"
-		with(response) {
-			status == 200
-		}
-	}
 
-	
-}
+
+ 	@Shared
+ 	def client
+
+ 	def setupSpec() {
+ 		Integer port = 8080
+ 		String actualPort = System.getProperty("server.port")
+
+ 		if (actualPort != null) {
+ 			port = Integer.valueOf(actualPort)
+ 		}
+
+ 		String basePath = "/wizard/"
+ 		String actualBasePath = System.getProperty("server.base")
+
+ 		if(actualBasePath != null){
+ 			basePath = actualBasePath
+ 		}
+
+ 		String baseHost = "localhost"
+ 		String actualHost = System.getProperty("server.host")
+
+ 		if(actualHost != null){
+ 			baseHost = actualHost
+ 		}
+
+
+ 		client = new RESTClient("http://" + baseHost + ":" + port + basePath)
+ 	}  
+
+ 	def "Homepage exists"() {
+ 		when: "a rest call is performed to the main page"
+ 		def response = client.get(path : "index.html")
+
+ 		then: "the correct message is expected"
+ 		with(response) {
+ 			status == 200
+ 		}
+ 	}
+
+ 	def "About Page exists"() {
+ 		when: "a rest call is performed to the about page"
+ 		def response = client.get(path : "about.html")
+
+ 		then: "the correct message is expected"
+ 		with(response) {
+ 			status == 200
+ 		}
+ 	}
+
+
+ }
